cae72fe4ff72e66b3f107618612e1763ae290969
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-08 19:34:20 -0500

    Got both get routes, post, put, and delete routes for categories written and working

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   }
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -29,7 +29,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   }
-})
+});
 
 module.exports = {
   Product,
