e1a65f090d7662ef8f28366047dd25b7b06eefd2
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-08 20:07:21 -0500

    Got get route for single product written and working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,9 +7,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   try {
-    // be sure to include its associated Category and Tag data
     const productData = await Product.findAll({
-      include: [{ model: Category }, {model: Tag, through: ProductTag}]
+      // include associated Category and Tag data
+      include: [{ model: Category }, {model: Tag, through: ProductTag }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -18,9 +18,23 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      //include associated Category and Tag data
+      include: [{ model: Category }, { model: Tag, through: ProductTag }]
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: 'No product exist with this id!'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
